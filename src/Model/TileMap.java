3e688fcd276e17834f9479c62ba8c890228b1c74
omar azam <umarazam98@gmail.com>
2018-10-29 17:06:05 -0400

    Completed tile map and view

--- src/Model/TileMap.java
+++ src/Model/TileMap.java
@@ -1,7 +1,7 @@
 package Model;
 //The TileMap class
 //This class controls the board and helps move around/perform actions for the entities within
-//Outline Author: Kyle Smith
+//Author: Omar Azam
 public class TileMap {
 	//The actual board
 	private Entity[][] board;
@@ -21,12 +21,30 @@ public class TileMap {
 	public boolean didWin()
 	{
 		boolean retVal = true;
+		for (int i = 0; i < Rows; i++)
+		{
+			for (int j = 0; j < Columns; j++)
+			{
+				if (board[i][j].getType() > 100)
+				{
+					retVal = false;
+				}
+			}
+		}
 		return retVal;
 	}
 	//this method remakes the board and populates it with empty space Entities
 	public void ResetBoard()
 	{
 		zombieWon = false;
+		board = new Entity[Rows][Columns];
+		for (int i = 0; i < Rows; i++)
+		{
+			for (int j = 0; j < Columns; j++)
+			{
+				addEntity(100,i,j);
+			}
+		}
 	}
 	//The getter for the boolean that determines if the user died
 	public boolean isZombieWon() {
@@ -39,6 +57,29 @@ public class TileMap {
 	public boolean addEntity(int type, int row, int column)
 	{
 		boolean retVal = false; //The returned boolean indicating success or failure
+		
+		if (board[row][column] == null || (board[row][column].getType() == 100 && row <= Rows && column <= Columns))
+		{
+			retVal = true;
+			switch (type)
+			{
+			case 1:
+				board[row][column] = new PeaShooter();
+				break;
+			case 2:
+				board[row][column] = new SunFlower();
+				break;
+			case 100:
+				board[row][column] = new Entity();
+				break;
+			case 101:
+				board[row][column] = new Zombie();
+				break;
+			}
+		}
+		board[row][column].setRow(row);
+		board[row][column].setColumn(column);
+		
 		return retVal;
 	}
 	//This method is mostly going to be called by the game when a Zombie kills a plant or vice versa
@@ -46,7 +87,19 @@ public class TileMap {
 	//their plants
 	public int removeEntity(int row, int column, boolean isUser)
 	{
-		int retVal = 0;	
+		int retVal = 0;
+		
+		if (getEntity(row,column).getType() > 100)
+		{
+			retVal = getEntity(row,column).getValue();
+		}
+		else
+		{
+			retVal = (int)(getEntity(row,column).getValue() * 0.15);
+		}
+		
+		board[row][column] = new Entity();
+		
 		return retVal;
 	}
 	//This method performs all the end turn methods on each entity
@@ -54,6 +107,27 @@ public class TileMap {
 	public int endTurn()
 	{
 		int retVal = 0;
+		for (int i = 0; i < Rows; i++)
+		{
+			for (int j = 0; j < Columns; j++)
+			{
+				board[i][j].turnPass();
+				if (board[i][j].getType()>100)
+				{
+					if (j-1 == 0)
+					{
+						zombieWon = true;
+					}
+					else if (((Zombie)board[i][j]).makemove(board[i][j-1]))
+					{
+						
+						 retVal += this.removeEntity(i,j-1,false);
+						 board[i][j-1] = board[i][j];
+						 board[i][j] = new Entity();
+					}
+				}
+			}
+		}
 		return retVal;
 	}
 	//Getter for an entity on the board
--- src/View/View.java
+++ src/View/View.java
@@ -8,10 +8,13 @@ import Model.TileMap;
 
 //The View class
 //This class controls output / input  to / from the user and also outputs the map
-//Outline Author: Kyle Smith
+//Author: Omar Azam
 public class View {
 	//The actual model that the view is displaying
 	private TileMap currentMap;
+	//A scanner for input and output.
+	Scanner scanner = new Scanner(System.in);
+
 	//Constructor
 	public View(TileMap newMap)
 	{
@@ -22,16 +25,68 @@ public class View {
 	//Return a char, representing what they look like.
 	public void showMap(int sun, int turn, int sunflowerCD, int peashooterCD)
 	{
-		
+		//I use this to output a list of all the current plants and zombies after the board
+		ArrayList<Zombie> zombies = new ArrayList<Zombie>();
+		ArrayList<Entity> plants = new ArrayList<Entity>();
+		//Clear is called so that the console looks blank
+		clear();
+		//These prints shows the different game statuses to the user
+		System.out.println("Sun: " + sun + "        Turn: " + turn);
+		System.out.println("SunFlower Cooldown: " + sunflowerCD + "     PeaShooter Cooldown: " + peashooterCD);
+		//The following creates a header for the table, so its easier for the user to interact with the board
+		System.out.print("          ");
+		for (int i = 0; i < currentMap.getColumns(); i++)
+		{
+			System.out.print("["+ i + "] ");
+		}
+		System.out.println();
+		//The following prints each row with a header at the front so its easier for the user to interact with the board
+		for (int i = 0; i < currentMap.getRows(); i++)
+		{
+			//Header
+			System.out.print("["+i+"]       ");
+			for (int j = 0; j < currentMap.getColumns(); j++)
+			{
+				//prints out each entities Display character for the current row column (i, j)
+				System.out.print("["+ currentMap.getEntity(i, j).getDisplay() + "] ");
+				if (currentMap.getEntity(i, j).getType() > 100)
+				{
+					zombies.add((Zombie)currentMap.getEntity(i, j));
+				}
+				else if (currentMap.getEntity(i, j).getType() < 100)
+				{
+					plants.add(currentMap.getEntity(i, j));
+				}
+			}
+			System.out.println();
+		}
+		//Prints a display of all the plants and zombies at the bottom, with their short toString statuses
+		System.out.println("Plants:");
+		for (int i = 0; i < plants.size(); i++)
+		{
+			System.out.println(plants.get(i).toStringShort());
+		}
+		System.out.println("Zombies:");
+		for (int i = 0; i < zombies.size(); i++)
+		{
+			System.out.println(zombies.get(i).toStringShort());
+		}
 	}
 	//This method clears the console
 	public void clear()
 	{
+		for (int i = 0; i < 30; i++) {
+			System.out.println();
+		}
 	}
 	//This will print the prompt then hold the console for user input and return whatever they type
 	public String getInput(String prompt)
 	{
 		String retVal = "";
+		
+		System.out.println(prompt);
+		retVal = scanner.nextLine();
+
 		return retVal;
 	}
 	//What this method does is get the user input for the x and y
@@ -41,10 +96,12 @@ public class View {
 	//and then print it to the console.
 	public void showEntity(int row, int column)
 	{
+		System.out.println(currentMap.getEntity(row, column).toString());
 	}
 	//Displays a string for the Game Class
 	public void showMessage(String message)
 	{
+		System.out.println(message);
 	}
 	//Allows the GameClass to change the currentMap
 	public void setCurrentMap(TileMap currentMap) {
